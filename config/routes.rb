eae181e13ad011452cd00b96358513f93b4a45c9
Amy Cen <cenamy@gmail.com>
2019-06-11 16:40:51 -0400

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/', to: 'static#index', as: 'main'
   resources :powers, only: [:index, :show]
   resources :heroines, only: [:index, :show, :new, :create]  
 end
