23a332331e6b3778bc329fd780ae3c9736d3c291
Amy Cen <cenamy@gmail.com>
2019-06-11 16:29:42 -0400

    done

--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,4 +1,17 @@
 class Heroine < ApplicationRecord
     belongs_to :power
     validates_uniqueness_of :super_name, :case_sensitive => false
+
+    def self.search(search)
+        if search
+            power = Power.where('lower(name) LIKE ?', "%#{search.downcase}%")
+            if power
+                self.where(power_id: power)
+            else
+                Heroine.all
+            end
+        else
+            Heroine.all
+        end
+    end
 end
